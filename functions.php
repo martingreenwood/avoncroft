a6d66a53f9f502201db80e58e009894ff0d09bbe
Martin Greenwood <martin@wearewbeard.com>
2018-05-10 11:24:31 +0100

    fixes

--- functions.php
+++ functions.php
@@ -7,6 +7,10 @@
  * @package avoncroft
  */
 
+@ini_set( 'upload_max_size' , '64M' );
+@ini_set( 'post_max_size', '64M');
+@ini_set( 'max_execution_time', '300' );
+
 if ( ! function_exists( 'avoncroft_setup' ) ) :
 	/**
 	 * Sets up theme defaults and registers support for various WordPress features.
